ef7f1d89905eb636c0cb94cc246c4bad3ba7bade
kkwrecks <pomodoromrtmap@gmail.com>
2021-05-26 00:44:51 +0800

    schema part2

---
+++ src/controllers/controllers.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { ProductSchema } from '../models/models'; //tip to avoid typos, key in first where we are extracting object from so we get prompts
+
+const Product = mongoose.model('Product', ProductSchema);
+
+export const addnewProduct = (req, res) => {
+    let newProduct = new Product(req.body); //whatever passed in req.body of request is now the Product
+
+    newProduct.save((err, Product) => {
+        if (err) {
+            res.send(err); //if there's error, print error to console
+        }
+        res.json(Product); //else print json Product that was entered into db
+    });
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import { addnewProduct } from '../controllers/controllers';
+
+const routes = (app) => {
+    app.route('/products')
+
+        //post endpoint
+        .post(addnewProduct);
+}
+
+export default routes;
