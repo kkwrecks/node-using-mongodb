8c0e33d509a10b595c421435874be9bc82ae2467
kkwrecks <pomodoromrtmap@gmail.com>
2021-05-26 01:46:03 +0800

    04CRUD endpoints

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,19 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct, 
+    getProducts, 
+    getProductWithID 
+} from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
+        //get all the products in the database
+        .get(getProducts) //dont add ; here else next line wont run
 
         //post endpoint
         .post(addnewProduct);
+
+    app.route('/products/:ProductID')
+        //to get specific product with an ID
+        .get(getProductWithID);
 }
 
 export default routes;
